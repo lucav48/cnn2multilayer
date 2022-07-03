e4890e946df0554944bc6d6c570e96d58193edf3
lucav48 <lucav48@gmail.com>
2022-07-03 14:57:49 +0200

    resnet101v2

--- data/CNN.py
+++ data/CNN.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.applications import ResNet50, VGG16, ResNet50V2
+from tensorflow.keras.applications import ResNet50, VGG16, ResNet50V2, ResNet101, ResNet101V2
 from tensorflow.keras.models import load_model
 from tensorflow.saved_model import LoadOptions
 
@@ -11,6 +11,10 @@ def get_model(model_name, model_path):
             return ResNet50(weights="imagenet")
         elif model_name == "ResNet50V2":
             return ResNet50V2(weights="imagenet")
+        elif model_name == "ResNet110":
+            return ResNet101(weights="imagenet")
+        elif model_name == "ResNet110V2":
+            return ResNet101V2(weights="imagenet")
     else:
         model = load_model(model_path)
     return model
